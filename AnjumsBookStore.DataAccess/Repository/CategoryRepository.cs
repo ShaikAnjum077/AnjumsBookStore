01db916b3b2afce6d60c87c7790d3a41523f33b7
Anjum <Anjum@LAPTOP-HH9URF57>
2021-11-11 15:52:38 +0530

    code changes

---
+++ AnjumsBookStore.DataAccess/Repository/CategoryRepository.cs
@@ -0,0 +1,23 @@
+﻿using AnjumsBookStore.DataAccess.Data;
+using AnjumsBookStore.DataAccess.Repository.IRepository;
+using AnjumsBookStore.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnjumsBookStore.DataAccess.Repository
+{
+    public class CategoryRepository : Repository<Category>, ICategoryRepository
+    {
+        // modify the database w/ the db context
+        private readonly ApplicationDbContext _db;      // get the db instance using the constructor and DI 
+        
+        public CategoryRepository(ApplicationDbContext db) : base(db)     // use hot keys C-T-O-R to build the constructor
+        {
+            _db = db;
+        }
+    }
+}
