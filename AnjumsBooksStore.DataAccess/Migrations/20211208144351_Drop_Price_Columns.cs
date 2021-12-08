3ab816aca9748dd014cbe54711be28b3ed4ccb0e
Anjum <Anjum@LAPTOP-HH9URF57>
2021-12-08 20:19:42 +0530

    removed price columns

---
+++ AnjumsBooksStore.DataAccess/Migrations/20211208144351_Drop_Price_Columns.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AnjumsBooksStore.DataAccess.Migrations
+{
+    public partial class Drop_Price_Columns : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Price",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "Price100",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "Price50",
+                table: "Products");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "Price",
+                table: "Products",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<double>(
+                name: "Price100",
+                table: "Products",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<double>(
+                name: "Price50",
+                table: "Products",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+    }
+}
